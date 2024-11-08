db488ba5d506d26421a4cd91f05a68c6f8174924
pabloabanpa <pabloaba>
2024-11-08 00:17:43 -0400

    update

--- frontend/views/registro_compras.php
+++ frontend/views/registro_compras.php
@@ -74,53 +74,54 @@ $mensuales = $mensualModel->getAllMensual(); // Obtiene todos los registros
             </div>
             
             <div id="compras-registradas">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>N°</th>
-                            <th>Ver Factura Electrónica</th>
-                            <th>NIT Proveedor</th>
-                            <th>Razón Social Proveedor</th>
-                            <th>Código de Autorización</th>
-                            <th>Número Factura</th>
-                            <th>Número DUI/DIM</th>
-                            <th>Fecha de Factura/DUI/DIM</th>
-                            <th>Importe Total Compra</th>
-                            <th>Descuentos, Bonificaciones y Rebajas Sujestas a IVA</th>
-                            <th>Importe Gift Card</th>
-                            <th>Importe Base Credito Fiscal</th>
-                            <th>Crédito Fiscal</th>
-                            <th>Tipo Compra</th>
-                            <th>Codigo de Control</th>
-                            <th>Estado de Consolidacion</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    
-                </table>
-            </div>
-            
-            <div id="confirmacion-compras" style="display: none;">
-                <!-- Contenido de la pestaña de Confirmación de Compras -->
-                <table>
-                    <thead>
-                        <tr>
-                            <th>N°</th>
-                            <th>Ver Factura Electrónica</th>
-                            <th>NIT Proveedor</th>
-                            <th>Razón Social Proveedor</th>
-                            <th>Código de Autorización</th>
-                            <th>Número Factura</th>
-                            <th>Número DUI/DIM</th>
-                            <th>Fecha de Factura/DUI/DIM</th>
-                            <th>Importe Total Compra</th>
-                            <th>Descuentos, Bonificaciones y Rebajas Sujestas a IVA</th>
-                            <th>Importe Gift Card</th>
-                            <th>Importe Base Credito Fiscal</th>
-                            <th>Crédito Fiscal</th>
-                            <th>Tipo Compra</th>
-                        </tr>
-                    </thead>
+            <table >
+        <thead>
+            <tr>
+                <th>NIT Proveedor</th>
+                <th>Razón Social</th>
+                <th>Código de Autorización</th>
+                <th>Número Factura</th>
+                <th>Número DUI/DIM</th>
+                <th>Fecha Factura</th>
+                <th>Importe Total Compra</th>
+                <th>Descuentos</th>
+                <th>Importe Gift Card</th>
+                <th>Importe Base Crédito Fiscal</th>
+                <th>Crédito Fiscal</th>
+                <th>Tipo Compra</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (!empty($mensuales)): ?>
+                <?php foreach ($mensuales as $mensual): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($mensual['nit_proveedor']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['razon_social_proveedor']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['codigo_autorizacion']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['numero_factura']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['numero_dui_dim']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['fecha_factura']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['importe_total_compra']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['descuentos']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['importe_gift_card']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['importe_base_credito_fiscal']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['credito_fiscal']); ?></td>
+                        <td><?php echo htmlspecialchars($mensual['tipo_compra']); ?></td>
+                        <td>
+                            <a href="ver_mensual.php?id=<?php echo $mensual['id']; ?>">Ver</a> |
+                            <a href="editar_mensual.php?id=<?php echo $mensual['id']; ?>">Editar</a> |
+                            <a href="eliminar_mensual.php?id=<?php echo $mensual['id']; ?>" onclick="return confirm('¿Estás seguro de eliminar este registro?');">Eliminar</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="13">No hay registros disponibles.</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
             </div>
         </section>
     </main>
